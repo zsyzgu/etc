92cec76aa6eae1af64761196303b63661f3be100
zsyzgu <zsyzgu@163.com>
2016-11-26 18:42:07 +0800

    >

--- main.py
+++ main.py
@@ -57,6 +57,8 @@ def main():
 							max_buy_a = item[1]
 				max_buy_a = min(max_buy_a, amou)
 				if max_buy - max_buy2 > dd and max_buy != buy_XLF:
+					print(max_buy)
+					print(buy_XLF)
 					buy_XLF = max_buy
 					print(say_add(getid(), 'XLF', "BUY", max_buy2 + 1, max_buy_a), file=exchange)
 					print(say_add(getid(), 'XLF', "SELL", max_buy, max_buy_a), file=exchange)
@@ -75,9 +77,136 @@ def main():
 							min_sell_a = item[1]
 				min_sell_a = min(min_sell_a, amou)
 				if min_sell2 - min_sell > dd and min_sell != sell_XLF:
+					print(min_sell)
+					print(sell_XLF)
 					sell_XLF = min_sell
 					print(say_add(getid(), 'XLF', "BUY", min_sell, max_buy_a), file=exchange)
 					print(say_add(getid(), 'XLF', "SELL", min_sell2 - 1, max_buy_a), file=exchange)
+
+
+
+
+			if data['type'] == 'book' and data['symbol'] == 'WFC':
+				buy_mess = data['buy']
+				max_buy = 0
+				max_buy_a = 0
+				max_buy2 = 0
+				for item in buy_mess:
+					if (item[0] > max_buy2):
+						max_buy2 = item[0]
+						if max_buy2 > max_buy:
+							tmp = max_buy2
+							max_buy2 = max_buy
+							max_buy = tmp
+							max_buy_a = item[1]
+				max_buy_a = min(max_buy_a, amou)
+				if max_buy - max_buy2 > dd and max_buy != buy_WFC:
+					print(max_buy)
+					print(buy_WFC)
+					buy_WFC = max_buy
+					print(say_add(getid(), 'WFC', "BUY", max_buy2 + 1, max_buy_a), file=exchange)
+					print(say_add(getid(), 'WFC', "SELL", max_buy, max_buy_a), file=exchange)
+
+				sell_mess = data['sell']
+				min_sell = 1000000000
+				min_sell_a = 0
+				min_sell2 = 1000000000
+				for item in sell_mess:
+					if (item[0] < min_sell2):
+						min_sell2 = item[0]
+						if min_sell2 < min_sell:
+							tmp = min_sell2
+							min_sell2 = min_sell
+							min_sell = tmp
+							min_sell_a = item[1]
+				min_sell_a = min(min_sell_a, amou)
+				if min_sell2 - min_sell > dd and min_sell != sell_WFC:
+					print(min_sell)
+					print(sell_WFC)
+					sell_WFC = min_sell
+					print(say_add(getid(), 'WFC', "BUY", min_sell, max_buy_a), file=exchange)
+					print(say_add(getid(), 'WFC', "SELL", min_sell2 - 1, max_buy_a), file=exchange)
+			if data['type'] == 'book' and data['symbol'] == 'GS':
+				buy_mess = data['buy']
+				max_buy = 0
+				max_buy_a = 0
+				max_buy2 = 0
+				for item in buy_mess:
+					if (item[0] > max_buy2):
+						max_buy2 = item[0]
+						if max_buy2 > max_buy:
+							tmp = max_buy2
+							max_buy2 = max_buy
+							max_buy = tmp
+							max_buy_a = item[1]
+				max_buy_a = min(max_buy_a, amou)
+				if max_buy - max_buy2 > dd and max_buy != buy_GS:
+					print(max_buy)
+					print(buy_GS)
+					buy_GS = max_buy
+					print(say_add(getid(), 'GS', "BUY", max_buy2 + 1, max_buy_a), file=exchange)
+					print(say_add(getid(), 'GS', "SELL", max_buy, max_buy_a), file=exchange)
+
+				sell_mess = data['sell']
+				min_sell = 1000000000
+				min_sell_a = 0
+				min_sell2 = 1000000000
+				for item in sell_mess:
+					if (item[0] < min_sell2):
+						min_sell2 = item[0]
+						if min_sell2 < min_sell:
+							tmp = min_sell2
+							min_sell2 = min_sell
+							min_sell = tmp
+							min_sell_a = item[1]
+				min_sell_a = min(min_sell_a, amou)
+				if min_sell2 - min_sell > dd and min_sell != sell_GS:
+					print(min_sell)
+					print(sell_GS)
+					sell_GS = min_sell
+					print(say_add(getid(), 'GS', "BUY", min_sell, max_buy_a), file=exchange)
+					print(say_add(getid(), 'GS', "SELL", min_sell2 - 1, max_buy_a), file=exchange)
+			if data['type'] == 'book' and data['symbol'] == 'MS':
+				buy_mess = data['buy']
+				max_buy = 0
+				max_buy_a = 0
+				max_buy2 = 0
+				for item in buy_mess:
+					if (item[0] > max_buy2):
+						max_buy2 = item[0]
+						if max_buy2 > max_buy:
+							tmp = max_buy2
+							max_buy2 = max_buy
+							max_buy = tmp
+							max_buy_a = item[1]
+				max_buy_a = min(max_buy_a, amou)
+				if max_buy - max_buy2 > dd and max_buy != buy_MS:
+					print(max_buy)
+					print(buy_MS)
+					buy_MS = max_buy
+					print(say_add(getid(), 'MS', "BUY", max_buy2 + 1, max_buy_a), file=exchange)
+					print(say_add(getid(), 'MS', "SELL", max_buy, max_buy_a), file=exchange)
+
+				sell_mess = data['sell']
+				min_sell = 1000000000
+				min_sell_a = 0
+				min_sell2 = 1000000000
+				for item in sell_mess:
+					if (item[0] < min_sell2):
+						min_sell2 = item[0]
+						if min_sell2 < min_sell:
+							tmp = min_sell2
+							min_sell2 = min_sell
+							min_sell = tmp
+							min_sell_a = item[1]
+				min_sell_a = min(min_sell_a, amou)
+				if min_sell2 - min_sell > dd and min_sell != sell_MS:
+					print(min_sell)
+					print(sell_MS)
+					sell_MS = min_sell
+					print(say_add(getid(), 'MS', "BUY", min_sell, max_buy_a), file=exchange)
+					print(say_add(getid(), 'MS', "SELL", min_sell2 - 1, max_buy_a), file=exchange)
+
 		except:
 			traceback.print_exc()
 
