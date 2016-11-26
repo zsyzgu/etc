7afee888cb15c54542f32469ff392c30833339d1
zsyzgu <zsyzgu@163.com>
2016-11-26 15:44:41 +0800

    .

--- main.py
+++ main.py
@@ -9,35 +9,34 @@ import json
 
 id = 0
 def getid():
-    global id
-    id += 1
-    return id
+	global id
+	id += 1
+	return id
 
 def connect():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("test-exch-westernjindynasty", 25000))
-    return s.makefile('w+', 1)
-
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(("test-exch-westernjindynasty", 25000))
+	return s.makefile('w+', 1)
 
 def main():
-    exchange = connect()
-    print(say_hello("ZSYZGU"), file=exchange)
+	exchange = connect()
+	print(say_hello("ZSYZGU"), file=exchange)
 
-    price = -1
-    po = 0
-    has_stock = False
+	price = -1
+	po = 0
+	has_stock = False
 
-    while True:
-        data = exchange.readline().strip()
-        data = json.loads(data)
-        if not data.has_key('type'):
-            continue
-        try:
-            if data['type'] == 'hello':
-                sym = data['symbols']
-                for item in sym:
-                    if item["symbol"] == "XLF":
-                    po = int(item["position"])
+	while True:
+		data = exchange.readline().strip()
+		data = json.loads(data)
+		if not data.has_key('type'):
+			continue
+		try:
+			if data['type'] == 'hello':
+				sym = data['symbols']
+				for item in sym:
+					if item["symbol"] == "XLF":
+						po = int(item["position"])
 			if data['type'] == 'book' and data['symbol'] == 'XLF':
 				buy_mess = data['buy']
 				sell_mess = data['sell']
@@ -64,8 +63,8 @@ def main():
 				if data['dir'] == "SELL":
 					id = getid()
 					print(say_add(id, data['symbol'], "SELL", price - 10, data['size']), file=exchange)
-        except:
-            traceback.print_exc()
+		except:
+			traceback.print_exc()
 
 if __name__ == "__main__":
-    main()
+	main()
