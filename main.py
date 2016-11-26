ed4bd77b31afca9caf43996f599b51dc204c563b
zsyzgu <zsyzgu@163.com>
2016-11-26 17:05:53 +0800

    >

--- main.py
+++ main.py
@@ -23,12 +23,16 @@ def main():
 	print(say_hello("ZSYZGU"), file=exchange)
 
 	price_XLF = -1
+	first_XLF = 1
 	po_XLF = 0
 	price_WFC = -1
+	first_WFC = 1
 	po_WFC = 0
 	price_GS = -1
+	first_GS = 1
 	po_GS = 0
 	price_MS = -1
+	first_MS = 1
 	po_MS = 0
 
 	while True:
@@ -59,7 +63,8 @@ def main():
 					buy_num = max(0, 10 - po_XLF)
 				else:
 					sell_num = max(0, 10 + po_XLF)
-				if price_XLF == -1:
+				if first_XLF == 1:
+					first_XLF = 0
 					print(say_add(getid(), 'XLF', "SELL", price_XLF + 10, sell_num), file=exchange)
 					print(say_add(getid(), 'XLF', "BUY", price_XLF - 10, buy_num), file=exchange)
 			if data['type'] == 'fill' and data['symbol'] == 'XLF':
@@ -92,7 +97,8 @@ def main():
 					buy_num = max(0, 10 - po_WFC)
 				else:
 					sell_num = max(0, 10 + po_WFC)
-				if po_WFC == 0:
+				if first_WFC == 1:
+					first_WFC = 0
 					print(say_add(getid(), 'WFC', "SELL", price_WFC + 10, sell_num), file=exchange)
 					print(say_add(getid(), 'WFC', "BUY", price_WFC - 10, buy_num), file=exchange)
 			if data['type'] == 'fill' and data['symbol'] == 'WFC':
@@ -121,7 +127,8 @@ def main():
 					price_MS = (max_buy + min_sell) / 2
 				sell_num = 10
 				buy_num = 10
-				if price_MS == -1:
+				if first_MS == 1:
+					first_MS = 0
 					buy_num = max(0, 10 - po_MS)
 				else:
 					sell_num = max(0, 10 + po_MS)
@@ -158,7 +165,8 @@ def main():
 					buy_num = max(0, 10 - po_GS)
 				else:
 					sell_num = max(0, 10 + po_GS)
-				if price_GS == -1:
+				if first_GS == 1:
+					first_GS = 0
 					print(say_add(getid(), 'GS', "SELL", price_GS + 10, sell_num), file=exchange)
 					print(say_add(getid(), 'GS', "BUY", price_GS - 10, buy_num), file=exchange)
 			if data['type'] == 'fill' and data['symbol'] == 'GS':
